954d32649f7c96af5dfc05cf9062dca987ae236b
farwaxzehra <farwaxzehra@gmail.com>
2025-09-10 18:10:53 +0500

    Create even_odd.py
    Added even/odd program

---
+++ even_odd.py
@@ -0,0 +1,10 @@
+#is number Even or Odd
+while True:
+ a = int(input("enter a num: "))
+
+ if a % 2 == 0:
+    print("num is EVEN")
+ elif a % 2 != 0:
+    print("Num is ODD")
+ else:
+    print("Invalid")
