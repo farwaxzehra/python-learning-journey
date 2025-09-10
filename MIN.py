00004bd78bb160829b9ee8fc971db0f8073d3a45
farwaxzehra <farwaxzehra@gmail.com>
2025-09-10 18:14:45 +0500

    Create MIN.py
    Added a program to find Min of 2 numbers.

---
+++ MIN.py
@@ -0,0 +1,9 @@
+print("FINDING MIN OF 2 NUMBERS".center(70))
+a = int(input("a: enter a num: "))
+b = int(input("b: enter another num: "))
+if a < b:
+    print('a is MIN =', a)
+elif a > b:
+    print('b is MAX =', b)
+else:
+    print('error')
