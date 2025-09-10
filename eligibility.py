8315b863d79ca228db4deee2a50115839035237e
farwaxzehra <farwaxzehra@gmail.com>
2025-09-10 22:43:18 +0500

    Create eligibility.py
    Added a program to check if you are above than 18 to be eligible for driving or not.

---
+++ eligibility.py
@@ -0,0 +1,6 @@
+# Eligible yo drive or not
+age = int(input("Enter your age: "))
+if age >= 18:
+    print('you are eligible to drive. \nDrive safe:)')
+else:
+    print('You are not eligible to drive.')
